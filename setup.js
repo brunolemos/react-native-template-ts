12209faa1f5ca5199f5793dc9788d949e9f3de64
Bruno Lemos <brunohplemos@gmail.com>
2018-07-08 20:27:03 -0300

    Merge package.json

--- setup.js
+++ setup.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
+const templatePackageJSON = require('./package.template.json')
+
 // Utils
 const readFile = filename => fs.readSync(path.join(__dirname, filename))
 const deleteFile = filename => fs.unlinkSync(path.join(__dirname, filename))
@@ -8,6 +10,15 @@ const writeFile = (filename, data) =>
   fs.writeFileSync(path.join(__dirname, filename), data)
 
 console.log('🔄 Setting up...')
+
+// Merge package.json
+const originalPackageJSON = readFile('package.json')
+const updatedPackageJSON = Object.assign(
+  {},
+  originalPackageJSON,
+  templatePackageJSON
+)
+writeFile('package.json', JSON.stringify(updatedPackageJSON, null, 2))
 
 // Delete unnecessary files
 deleteFile('.flowconfig')
